85c903ee84bb20e2548ce88707c1cd5323dfd831
igorm84 <igormodesto.jf@hotmail.com>
2016-01-22 20:31:35 -0200

    MovimentosAede

---
+++ MovimentosAedes/Assets/Standard Assets/Scripts/Flying.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flying : MonoBehaviour {
+    public Rigidbody rb;
+
+    void FlyE()
+    {
+        rb.AddForce(0, 200f, 0);
+        //rb.AddForceAtPosition (new Vector3(1,1,1), 1f, 1f);
+        //transform.position = new Vector3(0,1f,0);
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        InvokeRepeating("FlyE", 0, 0.1f);
+    }
+    void FixedUpdate ()
+    {
+        rb.AddForce(Physics.gravity*3);
+        
+    }
+}
